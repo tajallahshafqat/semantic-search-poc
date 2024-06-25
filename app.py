b0a119631fa432450c26cf7a18b187bf9f6abe9c
Tajallah Shafqat <tajallahshafqat@gmail.com>
2024-06-25 22:29:46 +0500

    Added query functionality

--- app.py
+++ app.py
@@ -1,35 +1,124 @@
-import torch
-import json
-
+import sys
+import os
 from sentence_transformers import SentenceTransformer
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+import psycopg2
+import json
+import string
+from dotenv import load_dotenv
+
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+
+load_dotenv()
+
+DB_NAME = os.getenv('DB_NAME')
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
 
 embedder = SentenceTransformer("all-MiniLM-L6-v2")
 
-f = open("data/jobs.json")
-data = json.load(f)
+stopwords_nltk = stopwords.words('english')
 
-position_titles = list(map(lambda job: job["job_position_title"], data["data"]))
+def preprocess_job_description(job):
+    text = job['job_position_description'].lower()
+    # Tokenize keeping special terms intact
+    words = word_tokenize(text)
+    # Remove punctuation
+    words = [word.translate(str.maketrans('', '', string.punctuation)) for word in words]
+    words = [word for word in words if word and word not in stopwords_nltk]
+    lemmatizer = WordNetLemmatizer()
+    words = [lemmatizer.lemmatize(word) for word in words]
+    preprocessed_text = ' '.join(words)
+    job['job_position_description'] = preprocessed_text
+    return job
 
-corpus = []
-[corpus.append(x) for x in position_titles if x not in corpus]
+def create_embeddings(job):
+    job = preprocess_job_description(job)
+    corpus = job['job_position_title'] + ': ' + job['job_position_description']
+    embeddings = embedder.encode(corpus, convert_to_tensor=True).tolist()
+    return embeddings
 
-corpus_embeddings = embedder.encode(corpus, convert_to_tensor=True)
+def store_embeddings(job):
+    conn = psycopg2.connect(
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        host=DB_HOST,
+        port=DB_PORT
+    )
+    cursor = conn.cursor()
 
-queries = [
-    "Senior Mining Estimator",
-    "Mechanical Engineer",
-    "Project Manager"
-]
+    embeddings = create_embeddings(job)
 
-top_k = min(5, len(corpus))
-for query in queries:
-    query_embedding = embedder.encode(query, convert_to_tensor=True)
+    insert_query = """
+    INSERT INTO jobs_embeddings (embeddings, job_id)
+    VALUES (%s, %s)
+    ON CONFLICT (job_id) DO NOTHING
+    """
+    cursor.execute(insert_query, (embeddings, job["id"]))
 
-    similarity_scores = embedder.similarity(query_embedding, corpus_embeddings)[0]
-    scores, indices = torch.topk(similarity_scores, k=top_k)
+    conn.commit()
+    cursor.close()
+    conn.close()
 
-    print("\nQuery:", query)
-    print("Top 5 most similar sentences in corpus:")
+def find_job_by_id(jobs, job_id):
+    for job in jobs:
+        if job['id'] == job_id:
+            return job['job_position_title']
+    return None
 
-    for score, idx in zip(scores, indices):
-        print(corpus[idx], "(Score: {:.4f})".format(score))
+def query_embeddings(job):
+    conn = psycopg2.connect(
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        host=DB_HOST,
+        port=DB_PORT
+    )
+    cursor = conn.cursor()
+
+    embeddings = create_embeddings(job)
+
+    query = """
+    SELECT id, embeddings, job_id, embeddings <=> %s AS distance
+    FROM jobs_embeddings
+    ORDER BY embeddings <=> %s
+    LIMIT 5;
+    """
+    cursor.execute(query, (embeddings, embeddings))
+    results = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    for result in results:
+        job_title = find_job_by_id(jobs, result[2])
+        print(f"ID: {result[0]}, Job title: {job_title}, Distance: {result[3]}")
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1 and sys.argv[1] == 'store':
+        f = open("data/jobs.json")
+        data = json.load(f)
+
+        jobs = data["jobs"]
+        subset = jobs[:50000]
+
+        for job in subset:
+            store_embeddings(job)
+
+    elif len(sys.argv) > 1 and sys.argv[1] == 'query':
+        f = open("data/query.json")
+        data = json.load(f)
+
+        job = data["job"]
+        query_embeddings(job)
+
+    else:
+        print("Invalid argument. Please provide 'store' or 'query'.")
